4c301e965fe6cddf4217da8efda21406ce6440db
malexxim <malexxim@gmail.com>
2019-01-17 18:36:19 +0000

    Aula 1 game

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 const $root = document.querySelector("#root");
 const $memoryCard = document.createElement("article");
-const $icon = `
-<img src='img/icon-collabcode.png'
-alt='Gueio mascote da CollabCode'
-class='icon'
->
+const $memoryCardFront = document.createElement("article");
+
+const $iconCollab = `
+<img
+   src='img/icon-collabcode.png'
+  alt='Gueio mascote da CollabCode'
+  class='icon'>
+  
 `;
 
-$memoryCard.classList.add("memory-card");
+const $iconC = `
+<img
+  src='img/icon-c.png'
+  alt='Gueio mascote da CollabCode'
+  class='icon'>
+  `;
 
+$memoryCard.classList.add("memory-card");
 $root.insertBefore($memoryCard, null);
-$memoryCard.insertAdjacentHTML("afterbegin", $icon);
+
+$memoryCardFront.classList.add("memory-card");
+$memoryCardFront.classList.add("-front");
+$root.insertBefore($memoryCardFront, $memoryCard);
+
+$memoryCard.insertAdjacentHTML("afterbegin", $iconCollab);
+
+$memoryCardFront.insertAdjacentHTML("afterbegin", $iconC);
