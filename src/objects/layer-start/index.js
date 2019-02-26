fbdeff8d2d0fc91ac50c537fe8b5fa2666fd0b6f
malexxim <malexxim@gmail.com>
2019-02-26 04:00:25 +0000

    Aula 35 - 9 - Voltando de novo com Botão e camada

---
+++ src/objects/layer-start/index.js
@@ -0,0 +1,20 @@
+const layerStart = (function() {
+  const module = {};
+
+  module.render = content => {
+    const $transparencyLayer = transparencyLayer.render();
+    const $gameButton = gameButton.render(content);
+
+    return `
+    <div class="layer-start">
+      ${$transparencyLayer}
+      ${$gameButton}
+    
+    </div>
+    `;
+  };
+
+  return {
+    render: module.render
+  };
+})();
