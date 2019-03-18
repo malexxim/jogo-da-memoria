5c5f8ea31c2adc5c63f3bab4d262bd7388e893cf
malexxim <malexxim@gmail.com>
2019-03-18 20:49:45 +0000

    Aula 39 - Deixar o flat-button único

--- src/components/flat-button/index.js
+++ src/components/flat-button/index.js
@@ -1,33 +1,32 @@
 const flatButton = (function() {
   const module = {};
 
+  module._id = 0;
+
   module._style = active => {
     const $head = document.querySelector("head");
     const $style = document.createElement("style");
 
     $style.textContent = `
-    .flat-button {
-      background-color: ${active ? "#f25a70" : "#eae6da"};
-      color: ${active ? "#ffffff" : "#fffcee"};
-      font-size: 24px;
-      font-weight: bold;      
-      width: 50%;
-      height: 176px;
-      text-transform: uppercase;
-    }
-    .flat-button.-active {
-      background-color: #f25a70;
-      color: #ffffff;
-    }
+      .flat-button-${module._id} {
+        background-color: ${active ? "#f25a70" : "#eae6da"};
+        color: ${active ? "#ffffff" : "#fffcee"};
+        font-size: 24px;
+        font-weight: bold;      
+        width: 50%;
+        height: 176px;
+        text-transform: uppercase;
+      }    
     `;
 
     $head.insertAdjacentElement("beforeend", $style);
   };
 
   module.render = (content = "", active = false) => {
+    module._id++;
     module._style(active);
 
-    return `<button class="flat-button">${content}</button>`;
+    return `<button class="flat-button-${module._id}">${content}</button>`;
   };
 
   return {
