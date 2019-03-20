e837044f0110243433da7e6d5bebff1ab512df64
malexxim <malexxim@gmail.com>
2019-03-20 19:54:32 +0000

    Aula 40/1 - HTML do Avatar

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,9 +1,11 @@
 (function() {
   const $root = document.querySelector("#root");
 
-  const $loginButton = flatButton.render("Log in");
+  const $loginButton = flatButton.render("Log in", false);
   const $signupButton = flatButton.render("Sign up", true);
+  const $logoCollabcode = logoCollabcode.render();
 
   $root.insertAdjacentHTML("beforeend", $loginButton);
   $root.insertAdjacentHTML("beforeend", $signupButton);
+  $root.insertAdjacentHTML("beforeend", $logoCollabcode);
 })();
