60c70d88b7f68d7a3f02bd2722dbe6a6edcbcd2e
malexxim <malexxim@gmail.com>
2019-03-28 18:30:58 +0000

    Aula 42/1/2/3 - Adicionar na tela: Username, password e confirm password

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -6,12 +6,28 @@
   const $logoCollabcode = logoCollabcode.render();
   const $titleCollabcode = titleCollabcode.render("Welcome!");
   const $logoWrapper = logoWrapper.render($logoCollabcode, $titleCollabcode);
-  const $labelCollabcode = labelCollabcode.render("E-mail");
-  const $inputCollabcode = inputCollabcode.render();
+
+  const $emailLabel = labelCollabcode.render("E-mail");
+  const $emailInput = inputCollabcode.render();
+
+  const $usernameLabel = labelCollabcode.render("Username");
+  const $usernameInput = inputCollabcode.render();
+
+  const $passwordLabel = labelCollabcode.render("Password");
+  const $passwordInput = inputCollabcode.render();
+
+  const $confirmPasswordLabel = labelCollabcode.render("Confirm password");
+  const $confirmPasswordInput = inputCollabcode.render();
 
   $root.insertAdjacentHTML("beforeend", $loginButton);
   $root.insertAdjacentHTML("beforeend", $signupButton);
   $root.insertAdjacentHTML("beforeend", $logoWrapper);
-  $root.insertAdjacentHTML("beforeend", $labelCollabcode);
-  $root.insertAdjacentHTML("beforeend", $inputCollabcode);
+  $root.insertAdjacentHTML("beforeend", $usernameLabel);
+  $root.insertAdjacentHTML("beforeend", $usernameInput);
+  $root.insertAdjacentHTML("beforeend", $emailLabel);
+  $root.insertAdjacentHTML("beforeend", $emailInput);
+  $root.insertAdjacentHTML("beforeend", $passwordLabel);
+  $root.insertAdjacentHTML("beforeend", $passwordInput);
+  $root.insertAdjacentHTML("beforeend", $confirmPasswordLabel);
+  $root.insertAdjacentHTML("beforeend", $confirmPasswordInput);
 })();
