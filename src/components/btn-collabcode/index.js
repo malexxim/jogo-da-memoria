e341b8186e04ad2653bdb425a608208aab4684f9
malexxim <malexxim@gmail.com>
2019-03-28 21:34:06 +0000

    Aula 42/11 - CSS do botão Signup

--- src/components/btn-collabcode/index.js
+++ src/components/btn-collabcode/index.js
@@ -1,7 +1,32 @@
 const btnCollabcode = (function() {
   const module = {};
 
-  module.render = content => `<input type="submit" value=${content}>`;
+  module._style = () => {
+    const $head = document.querySelector("head");
+    const $style = document.createElement("style");
+
+    $style.textContent = `
+      .btn-collabcode {
+        height: 48px;
+        border-radius: 24px;
+        width: 100%;
+        color: #fff;
+        font-size: 14px;
+        text-transform: uppercase;
+        background-color: #f25a70;
+        cursor: pointer;
+
+      }
+    `;
+
+    $head.insertAdjacentElement("beforeend", $style);
+  };
+
+  module.render = content => {
+    module._style();
+
+    return `<input class="btn-collabcode" type="submit" value=${content}>`;
+  };
 
   return {
     render: module.render
