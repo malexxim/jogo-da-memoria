f1d10c8572688af91fa9eda86181aa7475be73b4
malexxim <malexxim@gmail.com>
2019-04-18 15:48:14 +0000

    V1 043 01 type dinâmico no input-collab

--- src/components/form-signup/index.js
+++ src/components/form-signup/index.js
@@ -16,16 +16,16 @@ const formSignup = (function() {
 
   module._children = () => {
     const $emailLabel = labelCollabcode.render("E-mail");
-    const $emailInput = inputCollabcode.render("example@gmail.com");
+    const $emailInput = inputCollabcode.render("example@gmail.com", "email");
 
     const $usernameLabel = labelCollabcode.render("Username");
     const $usernameInput = inputCollabcode.render("alex");
 
     const $passwordLabel = labelCollabcode.render("Password");
-    const $passwordInput = inputCollabcode.render("******");
+    const $passwordInput = inputCollabcode.render("******", "password");
 
     const $confirmPasswordLabel = labelCollabcode.render("Confirm password");
-    const $confirmPasswordInput = inputCollabcode.render("******");
+    const $confirmPasswordInput = inputCollabcode.render("******", "password");
 
     const $btn = btnCollabcode.render("Signup");
 
